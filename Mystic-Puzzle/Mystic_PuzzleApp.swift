3777594d7b681e995919f866355381b503aa87a3
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-21 15:39:10 +0800

    - initialized project

---
+++ Mystic-Puzzle/Mystic_PuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mystic_PuzzleApp.swift
+//  Mystic-Puzzle
+//
+//  Created by Ronjie Diafante Man-on on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Mystic_PuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ Mystic-Puzzle/View/Main View/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mystic-Puzzle
+//
+//  Created by Ronjie Diafante Man-on on 5/21/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Text("Mystic Puzzle")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
